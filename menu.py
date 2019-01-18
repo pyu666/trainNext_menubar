ef81ada797b3686cc0086ffa51459a6a43b72ac4
pyu666 <32445749+pyu666@users.noreply.github.com>
2019-01-18 22:54:01 +0900

    とりあえず動きそう

--- menu.py
+++ menu.py
@@ -9,22 +9,18 @@ class TrainNext(rumps.App):
     def __init__(self):
         super(TrainNext, self).__init__("TrainNext")
         rumps.debug_mode(True)
-        self.menu = ['Show text', 'Notification']
+        self.menu = ["その次の電車"]
+        self.menu["その次の電車"].add("hoge")
+        self.menu["その次の電車"].add("huga")
         self.enable_text = False
+        self.icon="./icon.png"
+        #self.menu=["hoge","huga"]
 
-    def chk_train(list,hour,min):
-        for train in list:
-            if train[2] < hour:
-                continue
-            if train[3] < min:
-                continue
-            return train[2],train[3],train[4],train[5]
-        return None
-
-    def chk_now_time():
+    @ rumps.timer(30)
+    def chk_now_time(self,sender):
         print("hoge")
         now = datetime.today()
-
+        today = date.today()
         hour = now.hour
         minute = now.minute
          # [0-6],[月-日]
@@ -35,33 +31,52 @@ class TrainNext(rumps.App):
             weekday = (now-timedelta(days=1)).weekday()
         else:
             weekday = now.weekday()
-        if(jpholiday.is_holiday(train_date)):
+
+        print("huga")
+        if(jpholiday.is_holiday(today)):
             weekday = 6 # 祝日の場合,祝日ダイヤにする
+
         if weekday == 6:
             today_list = hd_list
         elif weekday == 5:
             today_list = sd_list
         else:
             today_list = wd_list
+        #print(today_list)
+        first = chk_train(today_list,hour,minute+5)
+        print("getfirst")
+        second = chk_train(today_list,first[1],first[2]+1)
+        third = chk_train(today_list,second[1],second[2]+1)
+        print(first[2],second,third)
+        self.title = ("  "+str(first[2])+" "+str(first[3]))
+        self.menu["その次の電車"].clear()
+        self.menu["その次の電車"].add(str(second[1])+":"+str(second[2])+" "+str(second[3]))
+        self.menu["その次の電車"].add(str(third[1])+":"+str(third[2])+" "+str(third[3]))
 
-        first = chk_train(today_list,hour,minute)
-        second = chk_train(today_list,first[2],first[3])
-        third = chk_train(today_list,second[2],second[3])
-        print(first,second,third)
 
 
-    @ rumps.timer(1)
-    def update(self,_):
 
-        print("hoge")
-        chk_now_time()
-        self.title = str(first)
-        self.menu = [str(second), str(third)]
+#    @ rumps.timer(1)
+#    def update(self,_):
+#        print("hoge")
+#        chk_now_time()
+#        self.title = str(first)
+#        self.menu = [str(second), str(third)]
 
     # 終電までは日付変わってもそのままにしなければならない！
         # 日本一遅い終電は1時台，日本一は早い始発は4時台
         # なので0~3時までは前日の時刻表を参照する
 
+def chk_train(list,hour,minute):
+    print("piyo")
+    for train in list:
+        if train[1] < hour:
+            continue
+        if train[1] == hour and train[2] < minute:
+            continue
+        print("a",end = " ")
+        return train
+    return None
 if __name__ == "__main__":
 
     wd_list = webDriver.weekday_List
