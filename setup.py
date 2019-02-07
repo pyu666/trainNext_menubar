ccb446eab74c44bba2ae9a2696b8069f697ec421
pyu666 <32445749+pyu666@users.noreply.github.com>
2019-02-08 00:35:08 +0900

    一部修正

--- setup.py
+++ setup.py
@@ -8,16 +8,18 @@ Usage:
 from setuptools import setup
 
 APP = ['menu.py']
-DATA_FILES = ['./icon.png']
+DATA_FILES = ['icon.png','icon.icns']
 APP_NAME = 'QuickIR'
+#'includes': ['webDriver', 'jpholiday','requests'],
 OPTIONS = {
-'includes': ['webDriver', 'jpholiday','BeautifulSoup','requests'],
+
     'argv_emulation': False,
     'iconfile': 'icon.icns',
     'plist': {
         'LSUIElement': True    },
-    'packages': ['rumps','webDriver', 'jpholiday','BeautifulSoup','requests',"datetime",'re'],
-}
+    }
+    #'packages': ['rumps','webDriver', 'jpholiday','BeautifulSoup4','requests',"datetime",'re'],
+
 setup(
     app=APP,
     data_files=DATA_FILES,
